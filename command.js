93de7f0d99778ad48a01d2c1417920dcf618c211
qadeer-xmd <qadeerxmd@gmail.com>
2025-09-08 01:58:37 +0500

    Create command.js

---
+++ command.js
@@ -0,0 +1,24 @@
+var commands = [];
+
+// Command Register Function
+function cmd(info, func) {
+    let data = info;
+    data.function = func;
+
+    if (!data.dontAddCommandList) data.dontAddCommandList = false;
+    if (!info.desc) info.desc = '';
+    if (!data.fromMe) data.fromMe = false;
+    if (!info.category) data.category = 'misc';
+    if (!info.filename) data.filename = "Not Provided";
+
+    commands.push(data);
+    return data;
+}
+
+module.exports = {
+    cmd,
+    AddCommand: cmd,
+    Function: cmd,
+    Module: cmd,
+    commands,
+};
